10077dd68048f012a8f5834018dd82f527730b23
Rahulddh <rahulsinghddh19@gmail.com>
2026-05-02 11:17:01 +0530

    array function

--- Day2/day2.js
+++ Day2/day2.js
@@ -44,3 +44,42 @@ arr.filter(x=> typeof x === "number").map(y => y * 2).forEach(abc => console.log
 
 //console.log(arr.find(x =. x === "Rahul"));
 // Arrow function is represented by =>
+console.log(Math.random()); // 0-1
+console.log(Math.random()* 10); // 0-10
+
+console.log(Math.floor(Math.random() * 10)); // 0-10
+// floor -> rounds down to the nearest integer
+console.log(Math.floor(Math.random() * 10)+1); // 1-10
+console.log(Math.ceil(0.9)); //only give high value,  round upper to the nearest integer
+
+
+// ==/===
+//console.log(5 =='5'); //true
+//console.log(5 ==='5'); //faluse
+
+let ss= "5";
+console.log(Number(ss)); //5
+console.log(String(5)); //"5"
+
+let val = 0.6;
+console.log(parseInt(val)); // 0
+console.log(parseFloat(val)); // 0.6
+// floor give int - low int, parseint give string - int
+
+let colors = ["red", "green", "blue"];
+console.log(colors.indexOf("green")); //1
+console.log(colors.indexOf("yellow")); //-1, because it is not present in it.
+console.log(colors.includes("yellow")); //false
+console.log(colors.length); //3
+console.log(colors[0]); //red
+console.log(colors[colors.length -1]); //blue
+colors.push("yellow"); // add yellow to colors
+console.log(colors); //["red", "green", "blue", "yellow"]
+colors.pop();
+console.log(colors); //["red", "green", "blue",]
+colors.unshift("purple"); // add purpule in the beginning of array
+console.log(colors);//["purple","red", "green", "blue"]
+colors.shift(); // remove the first elemnet from the array
+console.log(colors);//["red", "green", "blue"]
+
+
