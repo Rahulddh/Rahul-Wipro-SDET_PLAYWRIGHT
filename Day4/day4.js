3106233ef1bcf665c3856c564d52f8303f59f8db
Rahulddh <rahulsinghddh19@gmail.com>
2026-05-05 17:30:42 +0530

    api

--- Day4/day4.js
+++ Day4/day4.js
@@ -20,7 +20,7 @@ setInterval(() => {
  }, 3000);*/
  
 
-function getData(){
+/*function getData(){
     return new Promise( function (resolve, reject){
         if (true){
             resolve("Success!");   
@@ -37,4 +37,23 @@ getData().then((data) => {
 }).catch((error) => {
     console.error("Error:", error);
 });
+
+fetch("https://jsonplaceholder.typicode.com/todos")
+    .then(res => res.json()) // res -> string -> json)
+    .then(data => data.filter(d => d.id % 2 === 0))
+    .then(data => data.map(d => (d.title = "Aaryan " + d.title)))
+    .then(data => console.log(data.join("\n")))
+    .catch(error => console.error("Error fetching data:", error));*/
+
+
+
+
+
+    fetch("https://api.restful-api.dev/objects/4")
+    .then(response => {
+        if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
+        return response.json();
+    })
+ .then(data => console.log(data))
+ .catch(err => console.error(err));
 
