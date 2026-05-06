3e85573ba72726a5c28f2c0a10c8cefa7d0e8155
Rahulddh <rahulsinghddh19@gmail.com>
2026-05-06 12:04:03 +0530

    opps:- abstraction,constructor,encapsulation,inheritance,Polymorphism

--- Day4/api.js
+++ Day4/api.js
@@ -1,81 +1,96 @@
-/*// ## CAT FACTS
+// // ## CAT FACTS
 
-fetch("https://catfact.ninja/facts")
-    .then(res => res.json())
-    .then(result => result.data) // get an array
-    .then(facts => facts.filter( f => f.length <50)) // filter the facts that is less then 50 words
-    .then(facts =>facts.map(f => f.fact))
-    .then(data => console.log(data))
-    .catch(err => console.log("erroe;", err))
+// fetch("https://catfact.ninja/facts")
+//     .then(res => res.json())
+//     .then(result => result.data) // get an array
+//     .then(facts => facts.filter( f => f.length <50)) // filter the facts that is less then 50 words
+//     .then(facts =>facts.map(f => f.fact))
+//     .then(data => console.log(data))
+//     .catch(err => console.log("erroe;", err))
 
-//2) Pokémon — PokeAPI
+async function cat() {
+    let url = await fetch("https://catfact.ninja/facts");
+    data = await url.json;
+    let f  = data.filter(function(d){
+        return( d.length<50);
+    })
+    let r = await f.map(function(d){
+        return d.fact;
+    });
+}
 
-fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
-    .then(res => res.json())
-    .then(acc=> acc.results)
-    .then(arr => arr.filter(f => f.name.startsWith("p") ))
-    .then(newarr => newarr.map(f => f.name))
-    .then(console.log);
+async function result() {
+    const 
+    
+}
+// //2) Pokémon — PokeAPI
+
+// fetch("https://pokeapi.co/api/v2/pokemon?limit=20")
+//     .then(res => res.json())
+//     .then(acc=> acc.results)
+//     .then(arr => arr.filter(f => f.name.startsWith("p") ))
+//     .then(newarr => newarr.map(f => f.name))
+//     .then(console.log);
 
 
-  //3 Space People — Open Notify
-  fetch("http://api.open-notify.org/astros.json")
-    .then(r => r.json())
-    .then(obj => obj.people)
-    .then(arr => arr.map(p => p.name))
-    .then( names => console.log(names.join("\n")));
+//   //3 Space People — Open Notify
+//   fetch("http://api.open-notify.org/astros.json")
+//     .then(r => r.json())
+//     .then(obj => obj.people)
+//     .then(arr => arr.map(p => p.name))
+//     .then( names => console.log(names.join("\n")));
 
-  //4Quotes — Quotable
- fetch("https://zenquotes.io/api/quotes")
-    .then(r => r.json())
-    .then(arr => arr.filter(q => q.q.length < 100))
-    .then(arr => arr.map(q => q.q))
-    .then(names => console.log(names.join("\n")))*/
-    .catch(console.error);
+//   //4Quotes — Quotable
+//  fetch("https://zenquotes.io/api/quotes")
+//     .then(r => r.json())
+//     .then(arr => arr.filter(q => q.q.length < 100))
+//     .then(arr => arr.map(q => q.q))
+//     .then(names => console.log(names.join("\n")))
+//     .catch(console.error);
 
-  //5. Universities — Hipolabs Universities API
-fetch("http://universities.hipolabs.com/search?country=India")
-    .then(r => r.json())
-    .then(arr => arr.filter(u => u.name.includes("Technology")))
-    .then(arr => arr.map(u => u.name))
-    .then(console.log);
+//   //5. Universities — Hipolabs Universities API
+// fetch("http://universities.hipolabs.com/search?country=India")
+//     .then(r => r.json())
+//     .then(arr => arr.filter(u => u.name.includes("Technology")))
+//     .then(arr => arr.map(u => u.name))
+//     .then(console.log);
 
- /*// 6.Dog Facts — Dog API
-fetch("https://dogapi.dog/api/v2/facts")
-  .then(r => r.json())
-  .then(obj => obj.data)
-  .then(arr => arr.map(d => d.attributes.body))
-  .then(console.log);
+//  // 6.Dog Facts — Dog API
+// fetch("https://dogapi.dog/api/v2/facts")
+//   .then(r => r.json())
+//   .then(obj => obj.data)
+//   .then(arr => arr.map(d => d.attributes.body))
+//   .then(console.log);
 
-  //7.Crypto — CoinGecko
-fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
-  .then(r => r.json())
-  .then(arr => arr) // already array
-  .then(arr => arr.filter(c => c.current_price > 1000))
-  .then(arr => arr.map(c => c.name))
-  .then(console.log);
+//   //7.Crypto — CoinGecko
+// fetch("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd")
+//   .then(r => r.json())
+//   .then(arr => arr) // already array
+//   .then(arr => arr.filter(c => c.current_price > 1000))
+//   .then(arr => arr.map(c => c.name))
+//   .then(console.log);
 
-  //8.Random Users — Random User Generator
-fetch("https://randomuser.me/api/?results=10")
-  .then(r => r.json())
-  .then(obj => obj.results)
-  .then(arr => arr.filter(u => u.gender === "female"))
-  .then(arr => arr.map(u => u.name.first))
-  .then(console.log);
+//   //8.Random Users — Random User Generator
+// fetch("https://randomuser.me/api/?results=10")
+//   .then(r => r.json())
+//   .then(obj => obj.results)
+//   .then(arr => arr.filter(u => u.gender === "female"))
+//   .then(arr => arr.map(u => u.name.first))
+//   .then(console.log);
 
-  //9Countries — REST Countries
-fetch("https://restcountries.com/v3.1/all?fields=name,population")
-  .then(r => r.json())
-  .then(d => Array.isArray(d) ? d : [])
-  .then(a => a.filter(c => c.population > 100000000))
-  .then(a => a.map(c => c.name.common))
-  .then(console.log)
-  .catch(console.error);
+//   //9Countries — REST Countries
+// fetch("https://restcountries.com/v3.1/all?fields=name,population")
+//   .then(r => r.json())
+//   .then(d => Array.isArray(d) ? d : [])
+//   .then(a => a.filter(c => c.population > 100000000))
+//   .then(a => a.map(c => c.name.common))
+//   .then(console.log)
+//   .catch(console.error);
 
-//   //10.Books — Open Library
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then(r => r.json())
-  .then(a => a.filter(p => p.userId === 1))
-  .then(a => a.map(p => p.title))
-  .then(console.log)
-  .catch(console.error);*/
+// //   //10.Books — Open Library
+// fetch("https://jsonplaceholder.typicode.com/posts")
+//   .then(r => r.json())
+//   .then(a => a.filter(p => p.userId === 1))
+//   .then(a => a.map(p => p.title))
+//   .then(console.log)
+//   .catch(console.error);
