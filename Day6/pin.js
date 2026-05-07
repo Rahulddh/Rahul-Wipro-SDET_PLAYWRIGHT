8032a00066a7b830d56182a1266c060747889e56
Rahulddh <rahulsinghddh19@gmail.com>
2026-05-07 17:27:23 +0530

    sir solution

--- Day6/pin.js
+++ Day6/pin.js
@@ -43,4 +43,21 @@ const doeach =e => console.log(e);
 // Heap --> box |   |  -->   doeach | saygoodbye | greetuser
 
 
-let 
+let age = 25; // number --> Stack
+let name = "Aryan"; // string --> Stack
+ 
+let user = { // object --> Heap
+    name: "Aryan",
+    age: 25
+}
+ 
+// Primitve copy (Stack)
+let x = 5;
+let y = x; // copy of value of x is stored in y
+ 
+// Refrence copy (Heap)
+let obj1 = { name: "Aryan" }; // heap
+let obj2 = obj1;
+ 
+obj2.name = "Arush";
+console.log(obj1.name); // obj1 and obj2 are referencing the same object in heap, so change in obj2 reflects in obj1
