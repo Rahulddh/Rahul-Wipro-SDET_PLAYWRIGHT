19c0f01a0c4a835c09c278e83bcf4d1738f235d9
Rahulddh <rahulsinghddh19@gmail.com>
2026-05-11 12:45:41 +0530

    function

--- Day 9/index.ts
+++ Day 9/index.ts
@@ -1,3 +1,15 @@
-const message: string = "Hello TypeScript";
-
+const message = "Hello TypeScript";
 console.log(message);
+
+const name : string = "Hello Rahul";
+console.log(name);
+
+let result: number| string;
+result = 10;
+result = "pass";
+console.log(result);
+function add(a: number, b: number): number {
+    return "Sum: " + (a + b);
+}
+ 
+console.log(add(5, 10));
